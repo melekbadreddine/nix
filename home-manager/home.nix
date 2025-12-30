3756bf20fbd5d0ae7865c31a100ec21d176a0192
melekbadreddine <mbadreddine5@gmail.com>
2025-12-30 15:42:31 +0100

    Add session variables and improve bash configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,6 +4,11 @@
   home.username = "melek";
   home.homeDirectory = "/home/melek";
 
+  home.sessionVariables = {
+    _ZO_DOCTOR = "0";
+    PATH = "$HOME/.cargo/bin:$HOME/.bun/bin:$PATH";
+  };
+
   home.packages = with pkgs; [
     # Languages & Runtimes
     go
@@ -31,19 +36,17 @@
       ll = "ls -alF";
       la = "ls -A";
       l = "ls -CF";
+      nix-switch = "home-manager switch --flake ~/.config/nix#melek";
     };
     
     # This keeps Ubuntu colors and logic
     bashrcExtra = ''
-      # Recovering your prompt
+      # Recovering prompt
       if [ "$color_prompt" = yes ]; then
           PS1=''${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
       else
           PS1=''${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
       fi
-      
-      # Ensure cargo binaries are in PATH
-      export PATH="$HOME/.cargo/bin:$PATH"
     '';
   };
 
