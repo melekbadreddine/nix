1fabae1dca420d8e9b5d6cae23596bdaa8bff652
melekbadreddine <mbadreddine5@gmail.com>
2026-01-02 19:13:24 +0100

    Fix pkgs assignment in flake.nix to use system variable instead of stdenv.hostPlatform.system

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
   outputs = { nixpkgs, home-manager, fresh, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${stdenv.hostPlatform.system};
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations."melek" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
