78703ec3b46fe46cfb69141690ea21196b83f1d3
melekbadreddine <mbadreddine5@gmail.com>
2026-01-02 15:39:48 +0100

    Update flake.lock and refactor bash aliases for consistency and improved readability

--- home-manager/modules/shell/bash.nix
+++ home-manager/modules/shell/bash.nix
@@ -11,9 +11,10 @@
       showfiles = ''fd --type f --exclude "*.json" --exclude "*.lock" | while read -r file; do echo ""; echo "=== $file ==="; bat --color=always --style=plain "$file"; done'';
       
       # Nix management
-      nix-switch = "home-manager switch --flake ~/.config/nix#melek";
-      nix-update = "nix flake update ~/.config/nix && home-manager switch --flake ~/.config/nix#melek";
-      nix-edit = "fresh ~/.config/nix/";
+      nix-switch = "home-manager switch --flake $HOME/.config/nix#melek";
+      nix-update = "nix flake update $HOME/.config/nix && home-manager switch --flake $HOME/.config/nix#melek";
+      nix-edit = "fresh $HOME/.config/nix/";
+      nix-gc     = "nix-collect-garbage -d";
 
       # System info
       ports = "netstat -tulanp";
