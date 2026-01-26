a0a45d7f0f2640da8cdaa663a27deaf436f5b6ae
melekbadreddine <mbadreddine5@gmail.com>
2026-01-26 19:00:30 +0100

    Bump fresh-editor to v0.1.90

--- home-manager/modules/dev/fresh.nix
+++ home-manager/modules/dev/fresh.nix
@@ -2,10 +2,10 @@
   home.packages = [
     (pkgs.rustPlatform.buildRustPackage {
       pname = "fresh";
-      version = "0.1.86";
+      version = "0.1.90";
       src = fresh;
 
-      cargoHash = "sha256-I78sdf/p5TZZufMZF8rETRb91veklwNHHByHF1WbLgk=";
+      cargoHash = "sha256-tHuesoVUZ5OC9R662T4q0W8deXM/Gzvl0WEpHIltBOM=";
 
       nativeBuildInputs = [ 
         pkgs.pkg-config 
